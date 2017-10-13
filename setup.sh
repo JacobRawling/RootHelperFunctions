58e573d2ed322943af5c6cb8b4fb92f7027cb54a
JacobRawling <mrjacob3@gmail.com>
2017-10-13 11:24:10 +0100

    Added first set of code.

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "export ROOT_HELPER_FUNCS_PATH=$PWD" >> ~/.bash_profile
+export ROOT_HELPER_FUNCS_PATH=$PWD
+echo "export PYTHON_PATH=$ROOT_HELPER_FUNCS_PATH:$PYTHON_PATH" >> ~/.bash_profile
+export PYTHON_PATH=$ROOT_HELPER_FUNCS_PATH:$PYTHON_PATH
