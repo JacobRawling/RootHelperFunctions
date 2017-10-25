1e369ba2bfa9181f9b332aa0bcec59afe2259d60
jrawling <jrawling@cern.ch>
2017-10-25 14:20:20 +0100

    Fixed conditional statements in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
+echo "Configuring .bash_profile to append PYTHON PATH.." 
 echo "export ROOT_HELPER_FUNCS_PATH=$PWD" >> ~/.bash_profile
 export ROOT_HELPER_FUNCS_PATH=$PWD
 echo "export PYTHON_PATH=$ROOT_HELPER_FUNCS_PATH:$PYTHON_PATH" >> ~/.bash_profile
 export PYTHON_PATH=$ROOT_HELPER_FUNCS_PATH:$PYTHON_PATH
+echo "done." 
 
 ## Should exist 
-if [[ ! -v ATLAS_STYLE_PATH ]]; then
+if [ -z ${ATLAS_STYLE_PATH+x} ]; then
 	echo "CHECKING OUT ATLAS STYLE DEPENDENCY!" 
 	git clone https://github.com/JacobRawling/ATLASStyle.git ATLASStyle
 	source ATLASStyle/setup.sh 
+else
+ echo "ATLASStyle python package found."
 fi
